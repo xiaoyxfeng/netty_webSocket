dc5296fb298f67470dc71df36e5dc537f76a569d
笑逸新风 <xiaoyxfeng@126.com>
2022-06-16 12:50:49 +0800

    springboot_websocket

---
+++ src/main/java/com/example/springboot_websocket/SpringbootWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebSocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_websocket/SpringbootWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
