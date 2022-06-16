2085a63b12b6e5f4cea2ee7bfb657c8fcf34555c
笑逸新风 <xiaoyxfeng@126.com>
2022-06-16 13:11:18 +0800

    springboot_websocket

---
+++ src/main/java/com/example/springboot_websocket/WebSocketConfig.java
@@ -0,0 +1,13 @@
+package com.example.springboot_websocket;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description:
+ * @author: shijinxing
+ * @date: 2022-06-16 13:07
+ */
+
+@Configuration
+public class WebSocketConfig{
+}
